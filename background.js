9c381468e91c0c9506321cb68cd148056f9abee2
afmontarras <afmontarras@gmail.com>
2024-04-22 11:16:43 +0200

    dynamic popup with hidden status added

--- background.js
+++ background.js
@@ -8,146 +8,200 @@ globalThis.browser ??= chrome;
 
 // Function to check if a given URL is valid
 function isValidUrl(url) {
-  return /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/.test(url);
+  return /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/.test(
+    url
+  );
 }
 
-function initEnabledHostArray() {
+function initDisabledHostArray() {
   // Check if the disable_host array exists in local storage
-  browser.storage.local.get('enabled_host', (result) => {
-     if (result.enabled_host === undefined) {
-       // If it doesn't exist, initialize it as an empty array
-       browser.storage.local.set({ enabled_host: ['pubmed.ncbi.nlm.nih.gov','scholar.google.com'] });
-     }
+  browser.storage.local.get("disabled_host", (result) => {
+    if (result.disabled_host === undefined) {
+      // If it doesn't exist, initialize it as an empty array
+      browser.storage.local.set({
+        disabled_host: ["pubpeer.com", "peeriodicals.com", "www.google.com"],
+      });
+    }
   });
 }
- 
-// Call the function to initialize the disable_host array
-initEnabledHostArray();
 
-function addHostToEnabledArray(host) {
+// Call the function to initialize the disable_host array
+initDisabledHostArray();
+
+function addHostToDisabledArray(host) {
   return new Promise((resolve) => {
-      // Retrieve the current disable_host array
-      browser.storage.local.get('enabled_host', (result) => {
-        let EnabledHostArray = result.enabled_host || [];
-        // Add the new host to the array
-        EnabledHostArray.push(host);
+    // Retrieve the current disable_host array
+    browser.storage.local.get("disabled_host", (result) => {
+      let DisabledHostArray = result.disabled_host || [];
+      // Add the new host to the array
+      DisabledHostArray.push(host);
+      // Save the updated array back to local storage
+      browser.storage.local.set({ disabled_host: DisabledHostArray }, () => {
+        resolve(); // Resolve the promise when the operation is complete
+      });
+    });
+  });
+}
+
+function isHostInDisabledArray(host) {
+  return new Promise((resolve) => {
+    // Retrieve the current disabled_host array
+    browser.storage.local.get("disabled_host", (result) => {
+      let DisabledHostArray = result.disabled_host || [];
+      // Check if the host is in the array
+      resolve(DisabledHostArray.includes(host));
+    });
+  });
+}
+
+function removeHostFromDisabledArray(host) {
+  return new Promise((resolve) => {
+    // Retrieve the current disabled_host array
+    browser.storage.local.get("disabled_host", (result) => {
+      let DisabledHostArray = result.disabled_host || [];
+      // Find the index of the host to remove
+      const index = DisabledHostArray.indexOf(host);
+      if (index !== -1) {
+        // Remove the host from the array
+        DisabledHostArray.splice(index, 1);
         // Save the updated array back to local storage
-        browser.storage.local.set({ enabled_host: EnabledHostArray }, () => {
+        browser.storage.local.set({ disabled_host: DisabledHostArray }, () => {
           resolve(); // Resolve the promise when the operation is complete
         });
+      } else {
+        resolve(); // Resolve the promise even if the host is not found
+      }
+    });
+  });
+}
+
+function initHideHost() {
+  // Check if the hide_host exists in local storage
+  browser.storage.local.get("hide_host", (result) => {
+    if (result.hide_host === undefined) {
+      // If it doesn't exist, initialize it as false
+      browser.storage.local.set({
+        hide_host: false,
       });
+    }
   });
 }
 
-function isHostInEnabledArray(host) {
+// Call the function to initialize the hidden_host
+initHideHost();
+
+function setHideHost(value) {
   return new Promise((resolve) => {
-      // Retrieve the current enabled_host array
-      browser.storage.local.get('enabled_host', (result) => {
-        let EnabledHostArray = result.enabled_host || [];
-        // Check if the host is in the array
-        resolve(EnabledHostArray.includes(host));
-      });
+    chrome.storage.local.set({ hide_host: value }, () => {
+      resolve();
+    });
   });
 }
 
-function removeHostFromEnabledArray(host) {
+function getHideHost() {
   return new Promise((resolve) => {
-     // Retrieve the current enabled_host array
-     browser.storage.local.get('enabled_host', (result) => {
-       let EnabledHostArray = result.enabled_host || [];
-       // Find the index of the host to remove
-       const index = EnabledHostArray.indexOf(host);
-       if (index !== -1) {
-         // Remove the host from the array
-         EnabledHostArray.splice(index, 1);
-         // Save the updated array back to local storage
-         browser.storage.local.set({ enabled_host: EnabledHostArray }, () => {
-           resolve(); // Resolve the promise when the operation is complete
-         });
-       } else {
-         resolve(); // Resolve the promise even if the host is not found
-       }
-     });
+    chrome.storage.local.get("hide_host", function (result) {
+      const hideValue = result.hide_host === true;
+      resolve(hideValue);
+    });
   });
 }
-
 
 // Listener for messages from the background script or content scripts
 const listener = async (msg) => {
   const { name, host, tabId } = msg;
-    if (name === 'hide') {
-    browser.runtime.sendMessage({ name: 'close_window' });
+  if (name === "hide") {
+    // set hide true
+    setHideHost(true);
+    // remove comments
     runRemoveScript(tabId);
-  } 
-  else if (name === 'disable') {
-    await removeHostFromEnabledArray(host)
-    browser.runtime.sendMessage({ name: 'close_window' });
+    browser.runtime.sendMessage({ name: "close_window" });
+  } else if (name === "show") {
+    // set hide false
+    await setHideHost(false);
+    // show comments
+    showcomments(tabId);
+    browser.runtime.sendMessage({ name: "close_window" });
+  } else if (name === "disable") {
+    // set hide false (to not clash with popup msg)
+    setHideHost(false);
+    // add to disabled host list
+    addHostToDisabledArray(host);
+    // remove comments
     runRemoveScript(tabId);
-  } 
-  else if (name === 'enable') {
-    let alreadyenabled = await isHostInEnabledArray(host)
-    if (!alreadyenabled){
-      await addHostToEnabledArray(host)
-    }
-    browser.runtime.sendMessage({ name: 'close_window' });
+    browser.runtime.sendMessage({ name: "close_window" });
+  } else if (name === "enable") {
+    // remove host from disabled host list
+    await removeHostFromDisabledArray(host);
+    // show comments
     runDisplayScript(host, tabId);
-  }
-  else if (name === 'is_enable') {
-    let alreadyenabled = await isHostInEnabledArray(host)
-    if (alreadyenabled){
-      browser.runtime.sendMessage({ name: 'host_enable' });
+    browser.runtime.sendMessage({ name: "close_window" });
+  } else if (name === "is_enable") {
+    // get boolean for host enable and hidden status
+    let host_enabled = !(await isHostInDisabledArray(host));
+    let host_hidden = await getHideHost();
+    if (host_hidden) {
+      browser.runtime.sendMessage({ name: "host_hidden" });
+    } else if (host_enabled) {
+      browser.runtime.sendMessage({ name: "host_enable" });
     } else {
-      browser.runtime.sendMessage({ name: 'host_disable' });
+      browser.runtime.sendMessage({ name: "host_disable" });
     }
   }
-}
+};
 
+// Function to display content with a delay of 300 ms
+const showcomments = async (tabId) => {
+  setTimeout(async () => {
+    await browser.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["js/contentScript/content.js"],
+    });
+  }, 300);
+  await setHideHost(false);
+};
 // Function to execute scripts for displaying content
 const runDisplayScript = async (host, tabId) => {
   const scriptsToExecute = [
-    { file: 'js/contentScript/utils.js', target: { tabId: tabId } },
-    { file: 'js/contentScript/sanitizer.js', target: { tabId: tabId } },
-    { file: 'js/contentScript/domains.js', target: { tabId: tabId } },
-    { file: 'js/contentScript/browser.js', target: { tabId: tabId } },
-    { file: 'js/contentScript/element.js', target: { tabId: tabId } },
-    { file: 'js/contentScript/pubpeer.js', target: { tabId: tabId } }
+    { file: "js/contentScript/utils.js", target: { tabId: tabId } },
+    { file: "js/contentScript/sanitizer.js", target: { tabId: tabId } },
+    { file: "js/contentScript/domains.js", target: { tabId: tabId } },
+    { file: "js/contentScript/browser.js", target: { tabId: tabId } },
+    { file: "js/contentScript/element.js", target: { tabId: tabId } },
+    { file: "js/contentScript/pubpeer.js", target: { tabId: tabId } },
   ];
-  let show = await isHostInEnabledArray(host)
-  if (show) {
+  let host_enable = !(await isHostInDisabledArray(host));
+  if (host_enable) {
     for (const script of scriptsToExecute) {
       await browser.scripting.executeScript({
         target: script.target,
-        files: [script.file]
+        files: [script.file],
       });
     }
-    // Then Execute the content script with a delay of 300 ms
-    setTimeout(async () => {
-      await browser.scripting.executeScript({
-        target: { tabId: tabId },
-        files: ['js/contentScript/content.js']
-      });
-    }, 300)
+    // Then show comments
+    showcomments(tabId);
   }
-}
+};
 
 // Function to execute script for removing content
 const runRemoveScript = (tabId) => {
   browser.scripting.executeScript({
-      target: { tabId: tabId },
-      files: ['js/contentScript/removePubPeerMarks.js']
+    target: { tabId: tabId },
+    files: ["js/contentScript/removePubPeerMarks.js"],
   });
-}
+};
 
 // Initialize event listener for web navigation
 const initTabsEvent = () => {
   browser.webNavigation.onCompleted.addListener((details) => {
-      const { tabId, url } = details;
-      if (isValidUrl(url)) {
-        const host = url.indexOf('//') > -1 ? url.split('//')[1].split('/')[0] : '';
-        runDisplayScript(host, tabId);
-      }
+    const { tabId, url } = details;
+    if (isValidUrl(url)) {
+      const host =
+        url.indexOf("//") > -1 ? url.split("//")[1].split("/")[0] : "";
+      runDisplayScript(host, tabId);
+    }
   });
-}
+};
 
 // Initialize the service worker
 const init = () => {
--- js/popup.js
+++ js/popup.js
@@ -1,78 +1,77 @@
 // Browser Polyfill
 globalThis.browser ??= chrome;
 
-let host = '', state = '', tabId = null;
+let host = "",
+  state = "",
+  tabId = null;
 
 // Event listener for the DOMContentLoaded event, which fires when the initial HTML document has been completely loaded and parsed
 document.addEventListener("DOMContentLoaded", function (event) {
- setHost();
- initClickEvents();
- initMessagingEvents();
+  setHost();
+  initClickEvents();
+  initMessagingEvents();
 });
 
 // Function to set up click event listeners
 const initClickEvents = () => {
- document.addEventListener('click', eventListener);
+  document.addEventListener("click", eventListener);
 };
 
- // Listen for messages from the background script or other parts of the extension
+// Listen for messages from the background script or other parts of the extension
 const initMessagingEvents = () => {
- browser.runtime.onMessage.addListener(onMessage);
+  browser.runtime.onMessage.addListener(onMessage);
 };
 
 // Function to set the host variable based on the current active tab's URL
 const setHost = () => {
- browser.tabs.query({ active: true, currentWindow: true }, tabs => {
+  browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const { url, id } = tabs[0];
     tabId = id;
-    if (typeof url === 'string') {
-      host = url.indexOf('//') > -1 ? url.split('//')[1].split('/')[0] : '';
-      document.getElementById('host').innerText = host;
-      browser.runtime.sendMessage({ name: 'is_enable', host, tabId });
+    if (typeof url === "string") {
+      host = url.indexOf("//") > -1 ? url.split("//")[1].split("/")[0] : "";
+      browser.runtime.sendMessage({ name: "is_enable", host, tabId });
     }
- });
+  });
 };
 
 // Event listener for click events on the popup
 const eventListener = (e) => {
- const { id } = e.target;
- if (id === 'btn_hide') {
-      browser.runtime.sendMessage({ name: 'hide', host, tabId });
- } else if (id === 'btn_disable') {
-      browser.runtime.sendMessage({ name: 'disable', host, tabId });
- } else if (id === 'btn_enable') {
-      browser.runtime.sendMessage({ name: 'enable', host, tabId })
- } else if (id === 'btn_close') {
-      window.close();
-}
+  const { id } = e.target;
+  if (id === "btn_hide") {
+    browser.runtime.sendMessage({ name: "hide", host, tabId });
+  } else if (id === "btn_show") {
+    browser.runtime.sendMessage({ name: "show", host, tabId });
+  } else if (id === "btn_disable") {
+    browser.runtime.sendMessage({ name: "disable", host, tabId });
+  } else if (id === "btn_enable") {
+    browser.runtime.sendMessage({ name: "enable", host, tabId });
+  } else if (id === "btn_close") {
+    window.close();
+  }
 };
 
 // Function to handle messages received from the background script or other parts of the extension
 const onMessage = (msg) => {
-//  console.log(`Received message: ${JSON.stringify(msg)}`);
- const { name } = msg;
- if (name === 'close_window') {
-    // Close the popup window
+  const { name } = msg;
+  if (name === "close_window") {
     window.close();
- } else if (name === "host_enable") {
-    document.getElementById('state').innerText = 'ENABLED'
-    document.getElementById('actions').innerHTML = 
-    '<button id="btn_disable" class="btn btn-green">DISABLE COMMENTS</button>'+
-    '<button id="btn_hide" class="btn btn-green">HIDE COMMENTS (ephemeral)</button>'
- } else if (name === "host_disable") {
-    document.getElementById('state').innerText = 'DISABLED'
-    document.getElementById('actions').innerHTML = 
-    '<button id="btn_enable" class="btn btn-green">ENABLE COMMENTS</button>'
-}
+  } else if (name === "host_enable") {
+    document.getElementById("description").innerHTML =
+      "<p>This extension is ENABLED<br>on <b>" + host + "</b</p>";
+    document.getElementById("actions").innerHTML =
+      '<button id="btn_disable" class="btn btn-green">DISABLE</button>' +
+      '<button id="btn_hide" class="btn btn-green">HIDE COMMENTS ONE TIME</button>';
+  } else if (name === "host_disable") {
+    document.getElementById("description").innerHTML =
+      "<p>This extension is DISABLED<br>on <b>" + host + "</b</p>";
+    document.getElementById("actions").innerHTML =
+      '<button id="btn_enable" class="btn btn-green">ENABLE</button>';
+  } else if (name === "host_hidden") {
+    document.getElementById("description").innerHTML =
+      "<p>This extension is HIDDEN<br>On this tab only";
+    document.getElementById("actions").innerHTML =
+      '<button id="btn_show" class="btn btn-green">SHOW COMMENTS BACK</button>' +
+      '<button id="btn_disable" class="btn btn-green">DISABLE THE HOST</button>' +
+      '<p class="description"><br>The host is ' + host + '</p>';
+  }
 };
-
-function isHostInEnabledArray(host) {
-   return new Promise((resolve) => {
-       // Retrieve the current enabled_host array
-       browser.storage.local.get('enabled_host', (result) => {
-         let EnabledHostArray = result.enabled_host || [];
-         // Check if the host is in the array
-         resolve(EnabledHostArray.includes(host));
-       });
-   });
-}
